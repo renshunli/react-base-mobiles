a9f1438e9565def580ff852c48443769ca519fff
renshunli <254047050@qq.com>
2018-09-04 11:39:51 +0800

    1像素

--- src/util/rem.js
+++ src/util/rem.js
@@ -1,12 +1,12 @@
 /* 1像素 */
 var viewport = document.querySelector("meta[name=viewport]")
-if (window.devicePixelRatio == 1) {
+if (window.devicePixelRatio === 1) {
     viewport.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no')
 }
-if (window.devicePixelRatio == 2) {
+if (window.devicePixelRatio === 2) {
     viewport.setAttribute('content', 'width=device-width, initial-scale=0.5, maximum-scale=0.5, minimum-scale=0.5, user-scalable=no')
 }
-if (window.devicePixelRatio == 3) {
+if (window.devicePixelRatio === 3) {
     viewport.setAttribute('content', 'width=device-width, initial-scale=0.333333333, maximum-scale=0.333333333, minimum-scale=0.333333333, user-scalable=no')
 }
 
