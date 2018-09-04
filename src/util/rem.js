b533aada5ef905bcef791da59ff10fc6a00d79b5
renshunli <254047050@qq.com>
2018-09-04 11:38:11 +0800

    1像素边框

--- src/util/rem.js
+++ src/util/rem.js
@@ -1,3 +1,15 @@
+/* 1像素 */
+var viewport = document.querySelector("meta[name=viewport]")
+if (window.devicePixelRatio == 1) {
+    viewport.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no')
+}
+if (window.devicePixelRatio == 2) {
+    viewport.setAttribute('content', 'width=device-width, initial-scale=0.5, maximum-scale=0.5, minimum-scale=0.5, user-scalable=no')
+}
+if (window.devicePixelRatio == 3) {
+    viewport.setAttribute('content', 'width=device-width, initial-scale=0.333333333, maximum-scale=0.333333333, minimum-scale=0.333333333, user-scalable=no')
+}
+
 // 基准大小
 const baseSize = 32
 // 设置 rem 函数
